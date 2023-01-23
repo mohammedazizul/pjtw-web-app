4a2d9a4984b8f74762e1a606fee93ae54d4d3073
mohammedazizul <mohammedazizulhoquesheikh@gmail.com>
2023-01-23 14:06:02 +0800

    asset and test page added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import pjtw from "./assets/pjtw-logo.jpg";
+import Image from "react-bootstrap/Image";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        minHeight: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        fontFamily: "Courier New",
+        borderColor: "white",
+        backgroundColor: "#BD7E49",
+      }}
+    >
+      <Image
+        src={pjtw}
+        alt="logo of Perawan Juwita Terapi Warisan"
+        thumbnail="true"
+        style={{
+          height: "250px",
+          weight: "250px",
+        }}
+      />
+      <div
+        style={{
+          color: "#03755B",
+          fontFamily: "monospace",
+          padding: "10px",
+        }}
+      >
+        <h3>Coming Soon...</h3>
+      </div>
     </div>
   );
 }
